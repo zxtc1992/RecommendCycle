0a6bf97e1f866c79b35e9be832bd039a9053bbad
zouxin <zouxin@ueater.local>
2017-04-10 17:48:30 +0800

    无限轮播

---
+++ RecommendCycle/RecommendCycle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RecommendCycle
+//
+//  Created by zx on 2017/4/10.
+//  Copyright © 2017年 zx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RecommendCycle/RecommendCycle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RecommendCycle
+//
+//  Created by zx on 2017/4/10.
+//  Copyright © 2017年 zx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
